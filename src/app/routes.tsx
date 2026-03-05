e93dad4abc7495cf73267e5a0a4f6f86dddc2cd8
Eliaser Concha <eliaserdev99@gmail.com>
2026-03-04 21:36:58 -0300

    configuraciones adicionales

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,12 +1,16 @@
-import { Route, Switch } from "wouter"
+import { Router, Route, Switch } from "wouter"
+import { useHashLocation } from "wouter/use-hash-location"
 import LinksPage from "../features/links/pages/LinksPage"
 import UnderConstructionPage from "../features/under-construction/pages/UnderConstruction"
 
 export function Routes() {
   return (
-    <Switch>
+    <Router hook={useHashLocation}>
+      <Switch>
+        <Route path={"/"} component={UnderConstructionPage} />
         <Route path={"/links"} component={LinksPage} />
         <Route path={"*"} component={UnderConstructionPage} />
-    </Switch>
+      </Switch>
+    </Router>
   )
 }
